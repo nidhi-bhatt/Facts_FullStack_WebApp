128b75ab3c33ba50a491f192e1b53d08dc98ccbf
Nidhi Bhatt <itsnibhatt@gmail.com>
2023-06-17 16:55:00 +0530

    Lets do this

--- Main_contents/CSS-Section2/after-S03/script.js
+++ Main_contents/CSS-Section2/after-S03/script.js
@@ -1,7 +1,79 @@
-console.log("Hello");
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
 
-const btn = document.querySelector(".btn-open"); //select a button
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
+// Selecting DOM elements
+const btn = document.querySelector(".btn-open");
 const form = document.querySelector(".fact-form");
+const factsList = document.querySelector(".facts-list");
+
+// Create DOM elements: Render facts in list
+factsList.innerHTML = "";
+
+function createFactsList(dataArray) {
+  // factsList.insertAdjacentHTML("afterbegin", "<li>Jonas</li>");
+
+  const htmlArr = dataArray.map(
+    (fact) => `<li class="fact">
+    <p>
+    ${fact.text}
+      <a
+        class="source"
+        href="${fact.source}"
+        target="_blank"
+      >(Source)</a>
+    </p>
+    <span class="tag" style="background-color: ${
+      CATEGORIES.find((cat) => cat.name === fact.category).color
+    }">${fact.category}</span>
+  </li>`
+  );
+  const html = htmlArr.join("");
+  factsList.insertAdjacentHTML("afterbegin", html);
+}
+
+// Toggle form visibility
 btn.addEventListener("click", function () {
   if (form.classList.contains("hidden")) {
     form.classList.remove("hidden");
@@ -12,103 +84,112 @@ btn.addEventListener("click", function () {
   }
 });
 
-// javascript is like python, no need to declare datatype of variable
-// 7 types of data types in javascript
+console.log([7, 64, 6, -23, 11].filter((el) => el > 10));
+console.log([7, 64, 6, -23, 11].find((el) => el > 10));
 
-// falsy values : 0, '', null, undefined
-// truthy values: everything else....
+/*
+let votesInteresting = 23;
+let votesMindlowing = 5;
+const text = "Lisbon is the capital of Portugal";
 
-// let votesInteresting = 23;
-// let votesMindlowing = 5;
-// const text = "Lisbon is the capital of Portugal";
+votesInteresting = votesInteresting + 1;
+votesInteresting++;
+console.log(votesInteresting);
 
-// votesInteresting = votesInteresting + 1;
-// votesInteresting++;
-// console.log(votesInteresting);
+let totalUpvotes = votesInteresting + votesMindlowing;
+console.log("Upvotes:", totalUpvotes);
 
-// let totalUpvotes = votesInteresting + votesMindlowing;
-// console.log("Upvotes:", totalUpvotes);
+let votesFalse = 4;
+const isCorrect = votesFalse < totalUpvotes;
+console.log(isCorrect);
 
-// let votesFalse = 4;
-// const isCorrect = votesFalse < totalUpvotes;
-// console.log(isCorrect);
+console.log(parseInt("24.533ccc"));
+*/
 
-// console.log(parseInt("24.533ccc"));
+/*
+function calcFactAge(year) {
+  const currentYear = new Date().getFullYear();
+  const age = currentYear - year;
 
-// function calcFactAge(year) {
-//   const currentYear = new Date().getFullYear();
-//   const age = currentYear - year;
+  if (age >= 0) return age;
+  else return `Impossible year. Year needs to be less or equal ${currentYear}`;
+}
 
-//   if (age >= 0) return age;
-//   else return `Impossible year. Year needs to be less or equal ${currentYear}`;
-// }
+const age1 = calcFactAge(2015);
+console.log(age1);
+console.log(calcFactAge(2020));
+console.log(calcFactAge(2037));
 
-// const age1 = calcFactAge(2015);
-// console.log(age1);
-// console.log(calcFactAge(2020));
-// console.log(calcFactAge(2037));
+// const calcFactAge2 = (year) => 2022 - year;
+const calcFactAge2 = (year) =>
+  year <= new Date().getFullYear()
+    ? new Date().getFullYear() - year
+    : `Impossible year. Year needs to be less or equal ${new Date().getFullYear()}`;
 
-// console.log("Arrow operator start");
-// const calcFactAge2  = (year) => year <= new Date().getFullYear() ? new Date().getFullYear() - year : `Impossible year. Year needs to be less or equal ${new Date().getFullYear()}` ;
-// console.log(calcFactAge2(2026));
+console.log(calcFactAge2(2015));
+console.log(calcFactAge2(2037));
+*/
 
-// let votesInteresting = 20;
-// let votesMindlowing = 5;
+/*
+let votesInteresting = 20;
+let votesMindlowing = 5;
 
-// if (votesInteresting === votesMindlowing) {
-//   alert("This fact is equally interesting and mindblowing");
-// } else if (votesInteresting > votesMindlowing) {
-//   console.log("Interesting fact!");
-// } else if (votesInteresting < votesMindlowing) {
-//   console.log("Mindblowing fact!!");
-// }
+if (votesInteresting === votesMindlowing) {
+  alert("This fact is equally interesting and mindblowing");
+} else if (votesInteresting > votesMindlowing) {
+  console.log("Intersting fact!");
+} else if (votesInteresting < votesMindlowing) {
+  console.log("Mindblowing fact!!");
+}
 
-// // falsy values: 0, '', null, undefined
-// // truthy value: everything else...
-// if (votesMindlowing) {
-//   console.log("MINDBLOWING FACT!!!");
-// } else {
-//   console.log("Not so special...");
-// }
+// falsy values: 0, '', null, undefined
+// truthy value: everything else...
+if (votesMindlowing) {
+  console.log("MINDBLOWING FACT!!!");
+} else {
+  console.log("Not so special...");
+}
 
-// let votesFalse = 7;
-// const totalUpvotes = votesInteresting + votesMindlowing;
+let votesFalse = 7;
+const totalUpvotes = votesInteresting + votesMindlowing;
 
-// const message =
-//   totalUpvotes > votesFalse
-//     ? "The fact is true"
-//     : "Might be false, check more sources...";
-// // alert(message);
+const message =
+  totalUpvotes > votesFalse
+    ? "The fact is true"
+    : "Might be false, check more sources...";
+// alert(message);
 
-// const text = "Lisbon is the capital of Portugal";
-// const upperText = text.toUpperCase();
-// console.log(upperText);
+const text = "Lisbon is the capital of Portugal";
+const upperText = text.toUpperCase();
+console.log(upperText);
 
-// const str = `The current fact is "${text}". It is ${calcFactAge(
-//   2015
-// )} years old. It is probably ${
-//   totalUpvotes > votesFalse ? "correct" : "not true"
-// }.`;
-// console.log(str);
+const str = `The current fact is "${text}". It is ${calcFactAge(
+  2015
+)} years old. It is probably ${
+  totalUpvotes > votesFalse ? "correct" : "not true"
+}.`;
+console.log(str);
+*/
 
-const fact = ["Lisbon is the capital of Portugal", 2013, true];
+/*
+const fact = ["Lisbon is the capital of Portugal", 2015, true];
+console.log(fact[2]);
 console.log(fact.length);
-console.log(fact.length - 1);
-//true
+console.log(fact[fact.length - 1]);
+
 const [text, createdIn] = fact;
-const newfact = [...fact, "society"];
-// ... spread operator
-console.log(newfact);
+const newFact = [...fact, "society"];
+console.log(newFact);
 
 const factObj = {
   text: "Lisbon is the capital of Portugal",
+  category: "society",
   createdIn: 2015,
   isCorrect: true,
-  category: "society",
   createSummary: function () {
-    return `The fact ${
+    return `The fact "${
       this.text
-    } is from the category ${this.category.toUpperCase()}`;
+    }" is from the category ${this.category.toUpperCase()}`;
   },
 };
 
@@ -119,18 +200,78 @@ const { category, isCorrect } = factObj;
 console.log(category);
 console.log(factObj.createSummary());
 
-[2,3,4,5].forEach(function(el){
-  console.log(el);
-});
-const times10 = [2,4,5,6].map(function(el){
-  return el *10;
+// [2, 4, 6, 8].forEach(function (el) {
+//   console.log(el);
+// });
 
-});
-
+// const times10 = [2, 4, 6, 8].map(function (el) {
+//   return el * 10;
+// });
+const times10 = [2, 4, 6, 8].map((el) => el * 10);
 console.log(times10);
 
-//forEach: This iterates over a list and applies some operation with side effects to each list member (example: saving every list item to the database) and does not return anything.
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
 
+const allCategories = CATEGORIES.map((el) => el.name);
+console.log(allCategories);
+
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
+
+function calcFactAge(year) {
+  const currentYear = new Date().getFullYear();
+  const age = currentYear - year;
+
+  if (age >= 0) return age;
+  else return `Impossible year. Year needs to be less or equal ${currentYear}`;
+}
+
+const factAges = initialFacts.map((el) => calcFactAge(el.createdIn));
+console.log(factAges);
+console.log(factAges.join("-"));
+*/
+
+//forEach: This iterates over a list and applies some operation with side effects to each list member (example: saving every list item to the database) and does not return anything.
 //map: This iterates over a list, transforms each member of that list, and returns another list of the same size with the transformed members (example: transforming list of strings to uppercase). It does not mutate the array on which it is called (although the callback function may do so).
 
 //Array.forEach “executes a provided function once per array element.”
